072c75e0c768f3cd1c36b1d1c0e8e8e63c69c509
Ivan Kovalenko <g0g0lm0g0l@protonmail.com>
2024-03-09 21:59:37 +0100

    fragment_readed.xml tiene la estructura para visualizar los parametros que le pasan, la clase ReadedFragment.kt está implementada para recibir y mostrar los parametros

--- app/src/main/java/com/example/ivan_kovalenko_practicapmdm/viewmodel/ReadedFragment.kt
+++ app/src/main/java/com/example/ivan_kovalenko_practicapmdm/viewmodel/ReadedFragment.kt
@@ -1,60 +1,43 @@
 package com.example.ivan_kovalenko_practicapmdm.viewmodel
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.ivan_kovalenko_practicapmdm.R
+import com.example.ivan_kovalenko_practicapmdm.databinding.FragmentReadedBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ReadedFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ReadedFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentReadedBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_readed, container, false)
+    ): View {
+        _binding = FragmentReadedBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ReadedFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ReadedFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Obtener los datos pasados desde el fragmento anterior
+        val arguments = requireArguments()
+        val nombre = arguments.getString("nombre")
+        val apellidos = arguments.getString("apellidos")
+
+        // Mostrar los datos en los TextViews
+        binding.apply {
+            textViewNombre.text = nombre
+            textViewApellidos.text = apellidos
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
