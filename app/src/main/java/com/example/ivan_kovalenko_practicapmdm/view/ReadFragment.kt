a369ecee08fa9d41b5cf50fb65d7194b2370a497
Ivan Kovalenko <g0g0lm0g0l@protonmail.com>
2024-03-12 04:36:37 +0100

    funciona todo, están los dos botones de ReadFragment, ellos no funcionan

--- app/src/main/java/com/example/ivan_kovalenko_practicapmdm/view/ReadFragment.kt
+++ app/src/main/java/com/example/ivan_kovalenko_practicapmdm/view/ReadFragment.kt
@@ -5,20 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ivan_kovalenko_practicapmdm.AppSystemdApplication
+import com.example.ivan_kovalenko_practicapmdm.ArticleViewModelFactory
+import com.example.ivan_kovalenko_practicapmdm.R
+import com.example.ivan_kovalenko_practicapmdm.databinding.FragmentLikedBinding
 import com.example.ivan_kovalenko_practicapmdm.databinding.FragmentReadBinding
+import com.example.ivan_kovalenko_practicapmdm.view.adapter.ArticleAdapter
+import com.example.ivan_kovalenko_practicapmdm.viewmodel.HomeViewModel
 
 class ReadFragment : Fragment() {
 
     private var _binding: FragmentReadBinding? = null
     private val binding get() = _binding!!
 
+    private val articleViewModel: HomeViewModel by viewModels {
+        ArticleViewModelFactory((requireActivity().application as AppSystemdApplication).repository)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentReadBinding.inflate(inflater, container, false)
-        return binding.root
+        val view = binding.root
+
+        // Initialize RecyclerView and Adapter
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerNewsRead)
+        val adapter = ArticleAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        // Observe LiveData and update RecyclerView
+        articleViewModel.allArticles.observe(viewLifecycleOwner) { articles ->
+            articles?.let { adapter.submitList(it) }
+        }
+
+        return view
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
